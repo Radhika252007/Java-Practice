955a12280b50c06405a16475a2af6c3d1d73c725
Radhika <mukeshmadaan2025@gmail.com>
2025-09-16 10:23:06 +0530

    Added Practice Files of Lecture 7

---
+++ src/com/chitkara/Lecture7/MaxInEachRow.java
@@ -0,0 +1,21 @@
+package com.chitkara.Lecture7;
+
+public class MaxInEachRow {
+    public static void main(String[] args){
+        int[][] matrix = {
+                {1,32,3,4,5},
+                {2,3,76,3,2},
+                {3,32,56,22,2}
+        };
+        for(int[] row : matrix){
+
+            int max = row[0];
+            for(int i=1;i<row.length;i++){
+                if(row[i]>max){
+                    max = row[i];
+                }
+            }
+            System.out.println("Max Element in Row: " + max);
+        }
+    }
+}
