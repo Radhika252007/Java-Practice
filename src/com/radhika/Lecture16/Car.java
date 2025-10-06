10c19a0ae45eac44fa963e7334f6970118789542
Radhika <mukeshmadaan2025@gmail.com>
2025-10-06 11:20:42 +0530

    Added Practice files of Lecture 16

---
+++ src/com/radhika/Lecture16/Car.java
@@ -0,0 +1,4 @@
+package com.radhika.Lecture16;
+
+public class Car {
+}
---
+++ src/com/radhika/Lecture16/School.java
@@ -0,0 +1,26 @@
+package com.radhika.Lecture16;
+
+public class Student {
+    private String name;
+    private char grade;
+
+    public Student(String name, char grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+public class School extends Student{
+    
+}
